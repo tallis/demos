28ceca84ea3f158584881c0c1259957eb9669261
Rui Costa <ruicosta@ieee.org>
2015-07-28 23:44:39 +0100

    Removed old view template declaration

--- app.js
+++ app.js
@@ -7,14 +7,8 @@ var fs = require('fs')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); 
-
+// Template View Engine (.ejs -> .html)
 app.engine('.html', require('ejs').__express);
-
-// Optional since express defaults to CWD/views
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
